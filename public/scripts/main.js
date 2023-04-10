ced6fbd72c3ea703aab5c31399e9a2e812366a62
Clément <clement2104.boillot@gmail.com>
2023-04-11 00:05:46 +0200

    :sparkles: Add mandatory scroll-snap

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,6 @@
-const header = document.querySelector('header');
 const main = document.querySelector('main');
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      header.classList.add('scrolled')
-      observer.unobserve(main);
-    }
-  })
-});
-observer.observe(main);
-
 const scrollDownButton = document.getElementById('scroll-down-btn');
+
 scrollDownButton.addEventListener('click', () => {
-  header.classList.add('scrolled');
+  main.scrollIntoView({ behavior: 'smooth' });
 });
