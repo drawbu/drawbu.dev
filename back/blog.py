a42ef54fc7e35d7efba8e3533be7b177a032d375
Clément <clement2104.boillot@gmail.com>
2023-12-21 15:23:29 +0100

    Improve articles fetching with a property

--- back/blog.py
+++ back/blog.py
@@ -8,9 +8,6 @@ class BlogManager:
     def __init__(self, repo_url: str, repo_path: Optional[str] = None) -> None:
         self.repo_url: Final[str] = repo_url
         self.repo: str = repo_path or self.get_repo()
-        self.files: list[Path] = []
-        self.__add_folder(Path(self.repo))
-        print(self.files)
 
 
     def get_repo(self) -> str:
@@ -19,10 +16,18 @@ class BlogManager:
         subprocess.run(cmd, timeout=10)
         return repo
 
-    def __add_folder(self, folder: Path):
+    @property
+    def articles(self):
+        files: list[Path] = []
+        self.__add_folder(Path(self.repo), files)
+        return files
+
+    def __add_folder(self, folder: Path, files: list[Path]) -> list[Path]:
         for f in folder.iterdir():
             if f.is_file() and f.suffix == ".md":
-                self.files.append(f)
+                files.append(f)
 
             elif f.is_dir() and not f.name.startswith('.'):
-                self.__add_folder(f)
+                self.__add_folder(f, files)
+
+        return files
