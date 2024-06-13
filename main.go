7b54b01d97a58e4f547d6f4af89c91079fcf142b
Clément <clement2104.boillot@gmail.com>
2024-06-13 17:36:05 +0200

    move: rename `homepage` to `root`

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"app/pkg/app"
 	"app/pkg/routes/blog"
 	"app/pkg/routes/contact"
-	"app/pkg/routes/homepage"
+	"app/pkg/routes/root"
 	"app/pkg/routes/resume"
 )
 
@@ -18,7 +18,7 @@ var (
 func main() {
 	serv := app.Server{Port: 8080, Prod: prod == "true"}
 
-	home := homepage.Handler(staticDir)
+	home := root.Handler(staticDir)
 	serv.AddRoute("GET /", home.Render)
 	blog := blog.Handler(articlesDir)
 	serv.AddRoute("GET /blog/{article...}", blog.Render)
