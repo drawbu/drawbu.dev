0f9402d1848d40506105512dba443c6bba1ed7b6
Clément <clement2104.boillot@gmail.com>
2024-09-02 21:58:56 +0200

    feat: improve rundev script

--- flake.nix
+++ flake.nix
@@ -11,21 +11,32 @@
       let
         pkgs = import inputs.nixpkgs { inherit system; };
 
-        devbuild = pkgs.writeShellScriptBin "devbuild" ''
-          tailwindcss -i static/style.css -o static/generated.css && \
-          templ generate                                          && \
-          go build                                                && \
-          ./app
-        '';
-        rundev = pkgs.writeShellScriptBin "rundev" ''
-          if [ -z "$1" ]; then
-            ${devbuild}/bin/devbuild
-          elif [ "$1" = "--watch" ]; then
-            ${pkgs.fd}/bin/fd | ${pkgs.entr}/bin/entr -c -r ${devbuild}/bin/devbuild
-          else
-            echo "Usage: $0 [--watch]"
-          fi
-        '';
+        rundev = pkgs.writeShellApplication {
+          name = "rundev";
+          runtimeInputs = [
+            pkgs.fd
+            pkgs.entr
+            (pkgs.writeShellApplication {
+              name = "buildapp";
+              text = ''
+                tailwindcss -i static/style.css -o static/generated.css && \
+                templ generate                                          && \
+                go build                                                && \
+                ./app
+              '';
+            })
+          ];
+          text = ''
+            if [ -z "$1" ]; then
+              buildapp
+            elif [ "$1" = "--watch" ]; then
+              fd | entr -c -r buildapp
+            else
+              echo "Usage: $0 [--watch]"
+            fi
+          '';
+        };
+
       in
       rec {
         formatter = pkgs.alejandra;
